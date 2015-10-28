ef5d7b0845d91b2374806d287817628e1e0c41c5
Daiwei <daiweilu123@gmail.com>
2015-10-27 23:12:47 -0400

    Update

--- leetcode-js/221-maximum-square.js
+++ leetcode-js/221-maximum-square.js
@@ -1,30 +1,35 @@
 /**
- * @param  {character[][]} matrix
+ * @param {character[][]} matrix
  * @return {number}
  */
 var maximalSquare = function(matrix) {
-  if (!matrix.length || !matrix[0].length) {
-    return 0;
-  }
+    var h = matrix.length;
+    if (h === 0) return 0;
+    var w = matrix[0].length;
+    if (w === 0) return 0;
+    var dp = makeArray(h, () => makeArray(w, 0));
+    var x;
+    var y;
+    var l = 0;
 
-  var y = matrix.length;
-  var x = matrix[0].length;
-  var dp = Array(y);
-  var i;
-  for (i = 0; i < dp.length; i++) {
-    dp[i] = Array(x);
-  }
-  var answer = 0;
-
-  for (y = 0; y < dp.length; y++) {
-    for (x = 0; x < dp[0].length; x++) {
-      dp[y][x] = parseInt(matrix[y][x]);
-      if (y && x && dp[y][x]) {
-        dp[y][x] = Math.min(dp[y-1][x], dp[y][x-1], dp[y-1][x-1]) + 1;
-      }
-      answer = Math.max(answer, dp[y][x]);
+    for (y = 0; y < h; y++) {
+        for (x = 0; x < w; x++) {
+            dp[y][x] = parseInt(matrix[y][x]);
+            if (x && y && dp[y][x]) {
+                dp[y][x] = Math.min(dp[y-1][x], dp[y][x-1], dp[y-1][x-1]) + 1;
+            }
+            l = Math.max(dp[y][x], l);
+        }
     }
-  }
 
-  return answer * answer;
+    return l * l;
 };
+
+function makeArray(size, filler) {
+  var arr = Array(size);
+  var i;
+  for (i = 0; i < arr.length; i++) {
+    arr[i] = typeof filler === 'function' ? filler() : filler;
+  }
+  return arr;
+}
