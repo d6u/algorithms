c1f426c3df6ce0af9ae8c0ed0ffdec485a9b2ee4
Daiwei <daiweilu123@gmail.com>
2015-11-01 23:08:11 -0500

    Update

--- leetcode-js/117-populating-next-right-pointers-in-each-node-ii.js
+++ leetcode-js/117-populating-next-right-pointers-in-each-node-ii.js
@@ -11,42 +11,41 @@
  * @return {void} Do not return anything, modify tree in-place instead.
  */
 var connect = function(root) {
-  if (!root) return;
+    if (!root) return;
 
-  var aboveHead = root;
-  var aboveCurrent;
-  var head;
-  var current;
+    var aboveLeftMost = root;
+    var aboveCurrent;
+    var leftMost;
+    var current;
 
-  while (aboveHead) {
-    aboveCurrent = aboveHead;
+    while (aboveLeftMost) {
+        aboveCurrent = aboveLeftMost;
 
-    while (aboveCurrent) {
+        while (aboveCurrent) {
+            if (aboveCurrent.left) {
+                if (!leftMost) {
+                    leftMost = aboveCurrent.left;
+                    current = aboveCurrent.left;
+                } else {
+                    current.next = aboveCurrent.left;
+                    current = current.next;
+                }
+            }
 
-      if (aboveCurrent.left) {
-        if (head == null) {
-          head = aboveCurrent.left;
-          current = aboveCurrent.left;
-        } else {
-          current.next = aboveCurrent.left;
-          current = current.next;
+            if (aboveCurrent.right) {
+                if (!leftMost) {
+                    leftMost = aboveCurrent.right;
+                    current = aboveCurrent.right;
+                } else {
+                    current.next = aboveCurrent.right;
+                    current = current.next;
+                }
+            }
+
+            aboveCurrent = aboveCurrent.next;
         }
-      }
 
-      if (aboveCurrent.right) {
-        if (head == null) {
-          head = aboveCurrent.right;
-          current = aboveCurrent.right;
-        } else {
-          current.next = aboveCurrent.right;
-          current = current.next;
-        }
-      }
-
-      aboveCurrent = aboveCurrent.next;
+        aboveLeftMost = leftMost;
+        leftMost = null;
     }
-
-    aboveHead = head;
-    head = null;
-  }
 };
