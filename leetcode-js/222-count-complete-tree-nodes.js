b0e5aa6e1550885a3f381b27819f08c690dc7c12
Daiwei <daiweilu123@gmail.com>
2015-12-05 13:48:59 -0500

    Update

--- leetcode-js/222-count-complete-tree-nodes.js
+++ leetcode-js/222-count-complete-tree-nodes.js
@@ -9,42 +9,13 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var countNodes = function(root) {
-    var depth = 0;
-    var node = root;
-
-    while (node) {
-        depth += 1;
-        node = node.left;
-    }
-
-    if (depth === 0) {
-        return 0;
-    }
-
-    var left = 0;
-    var right = (1 << (depth - 1)) - 1;
-    var mid;
-
-    while (left <= right) {
-        mid = (left + right) >> 1;
-        if (getNode(root, mid, depth -1)) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-
-    return (1 << (depth - 1)) + right;
+var countNodes = function (root) {
+    var h = height(root);
+    return h === 0 ? 0 :
+        height(root.right) === h - 1 ? (1 << (h - 1)) + countNodes(root.right) :
+            (1 << (h - 2)) + countNodes(root.left);
 };
 
-function getNode(root, path, depth) {
-    while (depth-- && root) {
-        if (path & (1 << depth)) {
-            root = root.right;
-        } else {
-            root = root.left;
-        }
-    }
-    return root;
+function height(root) {
+    return root == null ? 0 : 1 + height(root.left);
 }
