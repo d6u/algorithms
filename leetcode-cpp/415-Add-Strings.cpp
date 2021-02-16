b83ca17f53dc00be4aa0234db2bbece25f2f4227
Daiwei Lu <d6u@users.noreply.github.com>
2021-02-15 17:15:00 -0800

    Create 415-Add-Strings.cpp

---
+++ leetcode-cpp/415-Add-Strings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int carry = 0;
+        string str;
+        for (int i = num1.size() - 1, j = num2.size() - 1; i >= 0 || j >= 0; i--, j--) {
+            int x = i >= 0 ? num1[i] - '0' : 0;
+            int y = j >= 0 ? num2[j] - '0' : 0;
+            int digit = (x + y + carry) % 10;
+            carry = (x + y + carry) / 10;
+            str = (char)('0' + digit) + str;
+        }
+        if (carry == 1) {
+            str = (char)('0' + carry) + str;
+        }
+        return str;
+    }
+};
