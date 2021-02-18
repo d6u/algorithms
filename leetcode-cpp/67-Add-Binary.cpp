01ac55b77e0808292dc47b1efb7b672952a0b32c
Daiwei Lu <d6u@users.noreply.github.com>
2021-02-17 21:13:08 -0800

    Create 67-Add-Binary.cpp

---
+++ leetcode-cpp/67-Add-Binary.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string result = "";
+        int carry = 0;
+        for (int i = a.size() - 1, j = b.size() - 1; i >= 0 || j >= 0; i--, j--) {
+            int x = i >= 0 ? a[i] - '0' : 0;
+            int y = j >= 0 ? b[j] - '0' : 0;
+            result.insert(0, 1, '0' + ((x + y + carry) & 1));
+            carry = (x + y + carry) >> 1;
+        }
+        if (carry == 1) {
+            result.insert(0, 1, '1');
+        }
+        return result;
+    }
+};
